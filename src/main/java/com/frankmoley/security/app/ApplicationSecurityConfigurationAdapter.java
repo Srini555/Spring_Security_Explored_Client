4babcf518be44f1916847c251ba030cb222a931e
amitaucs <the.amisoft@gmail.com>
2019-06-05 08:59:31 +0530

    Home page assess without password.

--- src/main/java/com/frankmoley/security/app/ApplicationSecurityConfigurationAdapter.java
+++ src/main/java/com/frankmoley/security/app/ApplicationSecurityConfigurationAdapter.java
@@ -1,9 +1,17 @@
 package com.frankmoley.security.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
@@ -20,5 +28,13 @@ public class ApplicationSecurityConfigurationAdapter  extends WebSecurityConfigu
                 .httpBasic();
     }
 
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("amit").password("mypass").roles("ADMIN","USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("joe").password("foo").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
 
 }
